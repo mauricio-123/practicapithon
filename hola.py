cd4e922df391ee8847188204390ec864140ed25e
mauricio palacios <mpalacios@coopebombas.com>
2022-08-29 10:44:42 -0500

    NTS3-se crea primer hola mundo

---
+++ hola.py
@@ -0,0 +1,14 @@
+#print("hola mauricio")
+'''
+hola
+como esta 
+''' 
+#entrada  
+s1 = int(input("digita numero"))
+s2 = int(input("digite segundo numero "))
+s3 = s1 + s2
+edad = 33
+estatura = 1.63
+hincha = False
+
+print("hola" , s3  )
